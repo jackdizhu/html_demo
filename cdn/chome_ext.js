face3621f20da3f4903cb49b0acdae8d120b8c3d
jackdizhu <376365334@qq.com>
2017-08-06 01:41:48 +0800

    Update chome_ext.js

--- cdn/chome_ext.js
+++ cdn/chome_ext.js
@@ -2,7 +2,7 @@
     window._chome_ext = {
         baidu:{
             R: 'baidu.com',
-            id:['content_right']
+            id:['#content_right','.fxuQda','.c-container<.c-icon-v3','.c-container<.c-icon-v2']
         }
     };
     var _this = document.querySelector('#_chome_ext');

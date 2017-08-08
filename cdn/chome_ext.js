bfbff680235897518ff083c8fdc1ec9706b0e3c8
jackdizhu <376365334@qq.com>
2017-08-08 11:06:02 +0800

    Update chome_ext.js

--- cdn/chome_ext.js
+++ cdn/chome_ext.js
@@ -3,6 +3,10 @@
         baidu:{
             R: 'baidu.com',
             id:['#content_right','.fxuQda','.c-container<.c-icon-v3','.c-container<.c-icon-v2','.c-container<.c-icon-v1']
+        },
+        liewen:{
+            R: 'liewen.cc',
+            id:['#cs_right_bottom']
         }
     };
     var _this = document.querySelector('#_chome_ext');

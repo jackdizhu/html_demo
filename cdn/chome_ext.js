f56058d97b4cf2df318ecb5998f289a16b6e7214
jackdizhu <376365334@qq.com>
2017-08-08 21:58:58 +0800

    Update chome_ext.js

--- cdn/chome_ext.js
+++ cdn/chome_ext.js
@@ -16,4 +16,21 @@
     var _this = document.querySelector('#_chome_ext');
     var str = JSON.stringify(window._chome_ext);
     _this.setAttribute('data-ext',str);
+    
+    // 改写window.open 方法
+    function edit_windowOpen() {
+        var _arg = arguments;
+        window.open = function () {
+            console.log('window.open');
+        }
+    }
+    edit_windowOpen();
+    // 改写onkeydow 方法
+    function edit_keyDow() {
+        document.onkeydow = function () {
+            var _arg = arguments;
+            console.log('onkeydow');
+        }
+    }
+    edit_keyDow();
 })();

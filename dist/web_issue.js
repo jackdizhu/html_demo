153447be1950cce863c882696892a40c763fe617
jackdizhu <376365334@qq.com>
2018-01-04 11:08:16 +0800

    js 方法库 更新

--- dist/web_issue.js
+++ dist/web_issue.js
@@ -44,3 +44,19 @@ let isThisHost = function() {
 	return is
 }
 isThisHost()
+
+// js 获取url get 参数
+let getRequestParams = function() {
+  // 获取url中"?"符后的字串
+  let url = location.search
+  let theRequest = {}
+  if (url.indexOf('?') != -1) {
+    let str = url.substr(1)
+    strs = str.split('&')
+    for(let i = 0; i < strs.length; i ++) {
+      theRequest[strs[i].split('=')[0]] = unescape(strs[i].split('=')[1])
+    }
+  }
+  return theRequest
+}
+getRequestParams()

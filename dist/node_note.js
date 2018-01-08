b112fb9454ff7dbf46fc84177eeb0e8a8f27136a
jackdizhu <376365334@qq.com>
2018-01-08 15:48:45 +0800

    es6 异步方法

--- dist/node_note.js
+++ dist/node_note.js
@@ -4,4 +4,26 @@ let a = () => {
   var arguments = process.argv.splice(2);
   return arguments
 }
-console.log(a())
+// console.log(a())
+
+// Promise 封装异步 方法
+const sleep = (time) => {
+  return new Promise((resolve, reject) => {
+      setTimeout(() => {
+          resolve('ok')
+      }, time)
+  })
+}
+// async 定义异步方法 await 等待异步返回
+const getJSON = async () => {
+  await sleep(3000)
+  return '11'
+}
+// async 定义异步方法 await 等待异步返回
+const makeRequest = async () => {
+  console.log(await getJSON())
+  return "done"
+}
+let _R = makeRequest().then(R => {
+  console.log(R)
+})

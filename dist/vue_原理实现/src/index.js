cb30a17e4df61431be044e94c4f3bfab1bd0bf22
jackdizhu <376365334@qq.com>
2018-06-20 16:58:18 +0800

    模版编译处理

--- dist/vue_原理实现/src/index.js
+++ dist/vue_原理实现/src/index.js
@@ -12,18 +12,18 @@ function Vue (data) {
   return new VUE(data)
 }
 // 构造函数
-function VUE ({data, dom, html}) {
+function VUE ({data, dom, html, fn}) {
   this.data = data
-  this.abduction(data, dom, html)
+  this.abduction({data, dom, html, fn})
   // 等待 abduction 数据劫持方法执行完成
   setTimeout(() => {
-    render(data, dom, html)
+    render({data, dom, html, fn})
   }, 0)
 }
 // render 优化
 let _time = null
 // 原型方法
-VUE.prototype.abduction = function (data, dom, html) {
+VUE.prototype.abduction = function ({data, dom, html, fn}) {
   for (let key in data) {
     let value = data[key]
     Object.defineProperty(data, key, {
@@ -48,7 +48,7 @@ VUE.prototype.abduction = function (data, dom, html) {
         clearTimeout(_time)
         setTimeout(() => {
           // 修改数据后 重新渲染数据
-          render(data, dom, html)
+          render({data, dom, html, fn})
         }, 0)
       }
     })
@@ -57,8 +57,31 @@ VUE.prototype.abduction = function (data, dom, html) {
   }
 }
 
+// 编译 dom 绑定事件
+function compileElement ({data, dom, _dom, html, fn}) {
+  if (_dom) {
+    let childNodes = _dom.childNodes
+    let _arr = []
+    _arr.slice.call(childNodes).forEach(function (node) {
+      let nodeAttrs = node.attributes
+      if (nodeAttrs) {
+        _arr.slice.call(nodeAttrs).forEach(function (nodeAttr) {
+          let attrName = nodeAttr.name
+          // 事件处理
+          if (attrName.indexOf('on') === 0) {
+            node.addEventListener(attrName.substring(2), fn[nodeAttr.value].bind(data), false)
+          }
+        })
+      }
+      if (node.childNodes && node.childNodes.length) {
+        compileElement({data, dom, _dom: node, html, fn})
+      }
+    })
+  }
+}
+
 // 数据渲染 事件绑定
-function render (data, dom, html) {
+function render ({data, dom, html, fn}) {
   let _renderTpl = html.renderTpl
 
   let _obj = {}
@@ -73,6 +96,16 @@ function render (data, dom, html) {
   // 缓存处理 dom 没有变化不赋值
   if (_renderTpl !== html.renderTpl) {
     dom.innerHTML = html.renderTpl
-    console.log(html, 'dom.innerHTML')
+
+    setTimeout(() => {
+      let _dom = document.createDocumentFragment()
+      let $el = dom.firstElementChild
+      _dom.appendChild($el)
+      // 编译 dom 绑定属性事件
+      // compileElement({data, dom, _dom, html, fn})
+
+      dom.appendChild(_dom)
+      console.log(html, 'dom.innerHTML')
+    }, 100)
   }
 }
